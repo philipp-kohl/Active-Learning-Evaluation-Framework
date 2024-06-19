f8f8a88cfccf2e65b6d25c45d7dc4f8c2a0a421d
Yokiball <yoka.kraemer@gmx.de>
2024-06-19 10:27:12 +0200

    Add kmeans teacher cluster based equal distribution

--- ale/teacher/exploration/k_means.py
+++ ale/teacher/exploration/k_means.py
@@ -3,6 +3,7 @@ from threading import Lock
 from typing import List, Dict, Any
 
 from numpy.linalg import norm
+import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -15,6 +16,22 @@ from ale.trainer.base_trainer import Predictor
 logger = logging.getLogger(__name__)
 lock = Lock()
 
+class ClusterDocument:
+    def __init__(self, idx: int, cluster_idx: int, distance: float):
+        self.idx = idx
+        self.cluster_idx = cluster_idx
+        self.distance = distance
+
+class ClusteredDocuments:
+    def __init__(self, documents: List[ClusterDocument], num_clusters: int):
+        self.clusters = np.arange(0,num_clusters)
+        self.documents = documents
+
+    def get_clustered_docs_by_idx(self, indices: List[int]) -> List[ClusterDocument]:
+        output = [doc for doc in self.documents if doc.idx in indices]
+        return output
+
+
 def tfidf_vectorize(id2text: Dict[int, str]):
     logger.info("Initial TFIDF-Vectorizing started.")
     vectorizer = TfidfVectorizer(analyzer="word", lowercase=True)
@@ -23,7 +40,7 @@ def tfidf_vectorize(id2text: Dict[int, str]):
     return X
 
 
-def cluster_documents(corpus: Corpus, k: int):
+def cluster_documents(corpus: Corpus, k: int) -> ClusteredDocuments:
     lock.acquire()
     try:
         logger.info(f"Initial k-means clustering with k={k} started.")
@@ -38,17 +55,18 @@ def cluster_documents(corpus: Corpus, k: int):
         npm_tfidf = X.todense()
         idx2distance = dict()
         centers = model.cluster_centers_
+        clustered_documents: List[ClusteredDocuments] = []
         for i in range(len(ids)):
             idx = ids[i]
             tfidf_vector = npm_tfidf[i]
             distances = [norm(center - tfidf_vector) for center in centers]
-            idx2distance[idx] = min(distances)
-
+            clustered_documents.append(ClusterDocument(idx,np.argmin(distances),np.min(distances)))
+        clustered_obj = ClusteredDocuments(clustered_documents,len(centers))
         logger.info("Initial k-means clustering done.")
     finally:
         lock.release()
 
-    return idx2distance
+    return clustered_obj
 
 
 @TeacherRegistry.register("k-means")
@@ -63,11 +81,57 @@ class KMeansTeacher(BaseTeacher):
             nlp_task=nlp_task
         )
         self.k = len(self.labels)
-        self.idx2distance = cluster_documents(corpus=corpus, k=self.k)
+        self.clustered_documents: ClusteredDocuments = cluster_documents(corpus=corpus, k=self.k)
 
     def propose(self, potential_ids: List[int], step_size: int,  budget: int) -> List[int]:
-        potential_distances = {idx: self.idx2distance[idx] for idx in potential_ids}
-        sorted_dict_by_score = sorted(potential_distances.items(), key=lambda x:x[1], reverse=True)
-        out_ids = [item[0] for item in sorted_dict_by_score[:step_size]]
+        docs = self.clustered_documents.get_clustered_docs_by_idx(potential_ids)
+        sorted_docs = docs.sort(key=lambda x: x.distance, reverse=True)
+        out_ids = [item.idx for item in sorted_docs[:step_size]]
 
         return out_ids
+
+
+@TeacherRegistry.register("k-means-cluster-based")
+class KMeansClusterBasedTeacher(BaseTeacher):
+    def __init__(self, corpus: Corpus, predictor: Predictor, seed: int, labels: List[Any], nlp_task: NLPTask):
+        super().__init__(
+            corpus=corpus,
+            predictor=predictor,
+            seed=seed,
+            labels=labels,
+            nlp_task=nlp_task
+        )
+        self.k = len(self.labels)
+        self.clustered_documents = cluster_documents(corpus=corpus, k=self.k)
+
+    def propose(self, potential_ids: List[int], step_size: int,  budget: int) -> List[int]:
+        docs = self.clustered_documents.get_clustered_docs_by_idx(potential_ids)
+        clusters = self.clustered_documents.clusters
+        docs_per_cluster = int(step_size/len(clusters)) # equal distribution
+        output_ids = []
+        empty_clusters = []
+
+        for cluster in clusters:
+            potential_docs_cluster = [doc for doc in docs if doc.cluster_idx==cluster]
+            sorted_docs_cluster = potential_docs_cluster.sort(key=lambda x: x.distance, reverse=True)
+            if len(sorted_docs_cluster) < docs_per_cluster: # less docs in cluster left than needed
+                output_ids.extend(sorted_docs_cluster)
+                empty_clusters.append(cluster)
+            else:
+                output_ids.extend(sorted_docs_cluster[:docs_per_cluster])
+
+        while step_size>len(output_ids) and len(empty_clusters)<len(clusters): # rest left
+            docs_per_rest_clusters = max(int(step_size-len(output_ids))/(len(clusters)-len(empty_clusters)),1) # equally distribute to not empty clusters
+            for cluster in clusters:
+                if cluster not in empty_clusters:
+                    potential_docs_cluster = [doc for doc in docs if doc.cluster_idx==cluster]
+                    sorted_docs_cluster = potential_docs_cluster.sort(key=lambda x: x.distance, reverse=True)
+                    if len(sorted_docs_cluster) < docs_per_rest_clusters: # less docs in cluster left than needed
+                        rest -= len(sorted_docs_cluster)
+                        output_ids.extend(sorted_docs_cluster)
+                        empty_clusters.append(cluster)
+                    else:
+                        rest -= docs_per_rest_clusters
+                        output_ids.extend(sorted_docs_cluster[:docs_per_rest_clusters])
+        
+        return output_ids
