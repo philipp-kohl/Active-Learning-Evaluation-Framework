a4d3f28cb47cba5fc5d644ac59cf8d4f19420b44
Philipp Kohl <p.kohl@fh-aachen.de>
2024-07-30 15:39:30 +0200

    Worked on feedback from pull request

--- ale/teacher/exploration/k_means.py
+++ ale/teacher/exploration/k_means.py
@@ -1,49 +1,15 @@
 import logging
 from typing import List, Any
 
-import numpy as np
-from numpy.linalg import norm
-from sklearn.cluster import KMeans
-
 from ale.config import NLPTask
 from ale.corpus.corpus import Corpus
 from ale.registry.registerable_teacher import TeacherRegistry
 from ale.teacher.base_teacher import BaseTeacher
-from ale.teacher.exploration.utils.cluster_helper import ClusterHelper, ClusteredDocuments, ClusterDocument
-from ale.teacher.exploration.utils.silhouette_helper import silhouette_analysis
+from ale.teacher.exploration.utils.cluster_helper import ClusterHelper
 from ale.teacher.teacher_utils import tfidf_vectorize, sentence_transformer_vectorize
 from ale.trainer.predictor import Predictor
 
 logger = logging.getLogger(__name__)
-
-
-def cluster_documents(corpus: Corpus, num_labels: int, seed: int) -> ClusteredDocuments:
-    data = corpus.get_all_texts_with_ids()
-    ids = list(data.keys())
-    X = tfidf_vectorize(texts=list(data.values()))
-
-    best_k: int = silhouette_analysis(num_labels, seed, "euclidian", X)
-
-    logger.info(f"Initial k-means clustering with k={best_k} started.")
-    # tfidf vectorize the dataset and apply k-means++
-    model = KMeans(n_clusters=best_k, init='k-means++',
-                   max_iter=300, n_init='auto')
-    model.fit(X)
-
-    # get the distance to the corresponding cluster centroid for each document
-    npm_tfidf = X.todense()
-    centers = model.cluster_centers_
-    clustered_documents: List[ClusterDocument] = []
-    for i in range(len(ids)):
-        idx = ids[i]
-        tfidf_vector = npm_tfidf[i]
-        distances = [norm(center - tfidf_vector) for center in centers]
-        clustered_documents.append(ClusterDocument(idx, np.argmin(distances), np.min(distances)))
-    clustered_docs = ClusteredDocuments(clustered_documents, len(centers))
-    logger.info("Initial k-means clustering done.")
-
-    return clustered_docs
-
 
 
 @TeacherRegistry.register("k-means")
@@ -57,15 +23,18 @@ class KMeansTeacher(BaseTeacher):
             labels=labels,
             nlp_task=nlp_task
         )
-        self.k = len(self.labels)
-        self.clustered_documents: ClusteredDocuments = cluster_documents(corpus=corpus, num_labels=self.k, seed=seed)
+        all_texts = list(corpus.get_all_texts_with_ids().values())
+        embeddings = tfidf_vectorize(all_texts)
+        self.cluster_helper = ClusterHelper(embeddings)
+        self.clustered_documents = self.cluster_helper.adaptive_cluster(corpus=corpus,
+                                                                        num_labels=len(self.labels),
+                                                                        seed=seed,
+                                                                        metric="euclidian")
 
     def propose(self, potential_ids: List[int], step_size: int, budget: int) -> List[int]:
         docs = self.clustered_documents.get_clustered_docs_by_idx(potential_ids)
         sorted_docs = docs.sort(key=lambda x: x.distance, reverse=True)
-        out_ids = [item.idx for item in sorted_docs[:step_size]]
-
-        return out_ids
+        return [item.idx for item in sorted_docs[:step_size]]
 
 
 @TeacherRegistry.register("k-means-cluster-based")
@@ -78,11 +47,16 @@ class KMeansClusterBasedTeacher(BaseTeacher):
             labels=labels,
             nlp_task=nlp_task
         )
-        self.num_labels = len(self.labels)
-        self.clustered_documents = cluster_documents(corpus=corpus, num_labels=self.num_labels, seed=seed)
+        all_texts = list(corpus.get_all_texts_with_ids().values())
+        embeddings = tfidf_vectorize(all_texts)
+        self.cluster_helper = ClusterHelper(embeddings)
+        self.cluster_helper.adaptive_cluster(corpus=corpus,
+                                             num_labels=len(self.labels),
+                                             seed=seed,
+                                             metric="euclidian")
 
     def propose(self, potential_ids: List[int], step_size: int, budget: int) -> List[int]:
-        pass#return propose_nearest_neighbors_to_centroids(self.clustered_documents, potential_ids, step_size, budget)
+        return self.cluster_helper.propose_nearest_neighbors_to_centroids(potential_ids, step_size, budget)
 
 
 @TeacherRegistry.register("k-means-cluster-based-bert-km")
